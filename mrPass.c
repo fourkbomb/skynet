520f5b2848309d86f1c4bb58fd0da0e78dfc5b4c
keepcalm444 <keepcalm444@gmail.com>
2015-05-09 18:09:11 +1000

    mrPass: implement spinning off all day erry day

--- mrPass.c
+++ mrPass.c
@@ -14,10 +14,12 @@
 #include "mechanicalTurk.h"
 
 action decideAction (Game g) {
+    int player = getWhoseTurn(g);
+    action nextAction;
+    nextAction.actionCode = PASS;
+    if (getStudents(g, player, STUDENT_MJ) > 0 && getStudents(g, player, STUDENT_MTV) > 0 && getStudents(g, player, STUDENT_MMONEY) > 0) {
+        nextAction.actionCode = START_SPINOFF;
+    }
 
-   
-   action nextAction;
-   nextAction.actionCode = PASS;
-   
-   return nextAction;
+    return nextAction;
 }
