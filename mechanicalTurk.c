889ec5ba9e8da89059173df79371e48cdf55738b
keepcalm444 <keepcalm444@gmail.com>
2015-05-25 19:36:06 +1000

    Game: build campuses, start spinoffs
    TODO: The retraining code may need some tweaking. When we've
    finished building campuses and arcs we want to spam spinoffs, but
    the code prefers BQN and BPS to MMONEY, MTV and MJ.

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -47,12 +47,42 @@
 static path *_findNextVacantARC(Game g, path *startingPath, 
 	char nextStep, int depth);
 static path *findNextVacantARC(Game g, path *startingPath);
+static action tryConvertTo(Game g, int studentTo, int *minTypes);
+static path *findNextVacantCampusSpot(Game g, path *startingPath);
+static path *_findNextVacantCampusSpot(Game g, path *startingPath, 
+    char nextStep, int depth);
+
 
 action decideAction (Game g) {
     int player = getWhoseTurn(g);
     action nextAction = {PASS, "", 0, 0};
+    if (getStudents(g, player, STUDENT_MJ) > 0
+        && getStudents(g, player, STUDENT_BQN) > 0
+        && getStudents(g, player, STUDENT_BPS)
+        && getStudents(g, player, STUDENT_MTV) < 1) {
+        // todo build a campus
+        path dest = {0};
+        if (player == UNI_A) {
+            strncpy(dest, UNI_A_CAMPUS_2, strlen(UNI_A_CAMPUS_2));
+        } else if (player == UNI_B) {
+            strncpy(dest, UNI_B_CAMPUS_2, strlen(UNI_B_CAMPUS_2));
+        } else if (player == UNI_C) {
+            strncpy(dest, UNI_C_CAMPUS_2, strlen(UNI_C_CAMPUS_2));
+        }
+        path *ptr = findNextVacantCampusSpot(g, &dest);
+        if (*ptr[0] == 'x') {
+            #ifdef AI_DEBUG
+            printf("Couldn't find any usable campuses!\n");
+            #endif
+        } else {
+            strncpy(dest, *ptr, strlen(*ptr));
+            free(ptr);
+            nextAction.actionCode = BUILD_CAMPUS;
+        }
+    }
     if (getStudents(g, player, STUDENT_BPS) > 0
-    	&& getStudents(g, player, STUDENT_BQN) > 0) {
+    	&& getStudents(g, player, STUDENT_BQN) > 0
+        && nextAction.actionCode == PASS) {
     	nextAction.actionCode = OBTAIN_ARC;
     	path dest = {0};
     	if (player == UNI_A) {
@@ -85,29 +115,14 @@ action decideAction (Game g) {
 
     	}
     	strncpy(nextAction.destination, dest, PATH_LIMIT-1);
-    } else if (getStudents(g, player, STUDENT_BPS) < 1) {
-    	// convert from * to BPS
-    	nextAction.actionCode = RETRAIN_STUDENTS;
-    	nextAction.disciplineTo = STUDENT_BPS;
-    	if (getStudents(g, player, STUDENT_MJ) >=
-    		getExchangeRate(g, player, STUDENT_MJ, STUDENT_BPS)) {
-    		nextAction.disciplineFrom = STUDENT_MJ;
-    	} else if (getStudents(g, player, STUDENT_MTV) >=
-    		getExchangeRate(g, player, STUDENT_MTV, STUDENT_BPS)) {
-    		nextAction.disciplineFrom = STUDENT_MTV;
-    	} else if (getStudents(g, player, STUDENT_MMONEY) >=
-    		getExchangeRate(g, player, STUDENT_MMONEY, STUDENT_BPS)) {
-    		nextAction.disciplineFrom = STUDENT_MMONEY;
-    	} else if (getStudents(g, player, STUDENT_BQN) >=
-    		getExchangeRate(g, player, STUDENT_BQN, STUDENT_BPS)+1) {
-    		// we want to have 1 bps left over so that
-    		// we can build an ARC.
-    		nextAction.disciplineFrom = STUDENT_BQN;
-    	} else {
-    		nextAction.actionCode = PASS;
-    	}
-    } else if (getStudents(g, player, STUDENT_BQN) < 1) {
-    	nextAction.actionCode = RETRAIN_STUDENTS;
+    }
+    if (getStudents(g, player, STUDENT_MJ) > 0
+        && getStudents(g, player, STUDENT_MMONEY) > 0
+        && getStudents(g, player, STUDENT_MTV) > 0
+        && nextAction.actionCode == PASS) {
+        nextAction.actionCode = START_SPINOFF;
+    } else if (nextAction.actionCode == PASS) {
+    	/*nextAction.actionCode = RETRAIN_STUDENTS;
     	nextAction.disciplineTo = STUDENT_BQN;
     	if (getStudents(g, player, STUDENT_MJ) >=
     		getExchangeRate(g, player, STUDENT_MJ, STUDENT_BQN)) {
@@ -123,7 +138,26 @@ action decideAction (Game g) {
     		nextAction.disciplineFrom = STUDENT_BPS;
     	} else {
     		nextAction.actionCode = PASS;
-    	}
+    	}*/
+        // we want to try and have at least one of each type
+        action r;
+        int types[] = {0, 1, 1, 1, 1, 1};
+        if (getStudents(g, player, STUDENT_BPS) < 1) {
+            r = tryConvertTo(g, STUDENT_BPS, types);
+        } else if (getStudents(g, player, STUDENT_BQN) < 1) {
+            r = tryConvertTo(g, STUDENT_BQN, types);
+        } else if (getStudents(g, player, STUDENT_MJ) < 1) {
+            r = tryConvertTo(g, STUDENT_MJ, types);
+        } else if (getStudents(g, player, STUDENT_MTV) < 1) {
+            r = tryConvertTo(g, STUDENT_MTV, types);
+        } else if (getStudents(g, player, STUDENT_MMONEY) < 1) {
+            r = tryConvertTo(g, STUDENT_MMONEY, types);
+        }
+        if (r.disciplineFrom != 0) {
+            nextAction.disciplineFrom = r.disciplineFrom;
+            nextAction.disciplineTo = r.disciplineTo;
+            nextAction.actionCode = r.actionCode;
+        }
     }
     /*
     if (getStudents(g, player, STUDENT_MJ) > 0 
@@ -133,6 +167,30 @@ action decideAction (Game g) {
     }*/
 
     return nextAction;
+}
+
+static action tryConvertTo(Game g, int studentTo, int *minTypes) {
+    action a = {RETRAIN_STUDENTS, "", 0, 0};
+    int successful = 0;
+    int studentFrom = 0;
+    for (int i = STUDENT_BPS; i <= STUDENT_MMONEY && successful != 1; i++) {
+        if (i != studentTo) {
+            if ((getStudents(g, getWhoseTurn(g), i) - getExchangeRate(g, getWhoseTurn(g), i, studentTo)) >= minTypes[i]) {
+                successful = 1;
+                studentFrom = i;
+            }
+        }
+    }
+    if (successful) {
+        //action a = {RETRAIN_STUDENTS, "", studentFrom, studentTo};
+        a.disciplineFrom = studentFrom;
+        a.disciplineTo = studentTo;
+        if (!isLegalAction(g, a)) {
+            a.disciplineFrom = 0;
+            a.disciplineTo = 0;
+        }
+    }
+    return a;
 }
 
 static path *_findNextVacantARC(Game g, path *startingPath, 
@@ -211,4 +269,85 @@ static path *findNextVacantARC(Game g, path *startingPath) {
 	printf("\n\nGoing to %s\n\n", *res);
 	#endif
 	return res;
+}
+
+static path *_findNextVacantCampusSpot(Game g, path *startingPath, 
+    char nextStep, int depth) {
+    path temp = {0};
+    strncat(&(temp[0]), *startingPath, strlen(*startingPath));
+    strncpy(&temp[strlen(*startingPath)], &nextStep, 1);
+    path *result;
+    int done = 0;
+    #ifdef AI_DEBUG
+    printf("%s => %s %d\n", *startingPath, temp, depth);
+    #endif
+    if (depth < 25) {
+        if (getCampus(g, temp) == VACANT_VERTEX) {
+            action checkIsOk = {BUILD_CAMPUS, "", 0, 0};
+            #ifdef AI_DEBUG
+            printf("found a vacant vertex! ");
+            #endif
+            strncpy(checkIsOk.destination, temp, strlen(temp));
+            if (isLegalAction(g, checkIsOk)) {
+                #ifdef AI_DEBUG
+                printf("it's legal! hooray!\n");
+                #endif
+                result = (path*)strndup(temp, PATH_LIMIT);
+                done = 1;
+            } else {
+                #ifdef AI_DEBUG
+                printf("it's illegal :(\n");
+                #endif
+                strncpy(temp, "x\0", 2);
+                result = (path*)strndup(temp, PATH_LIMIT);
+            }
+        }
+        if (!done && getARC(g, temp) == getWhoseTurn(g)) {
+            // keep going down this path
+            #ifdef AI_DEBUG
+            printf("found an arc owned by me, following: %d\n",
+                depth+1);
+            #endif
+            result = _findNextVacantCampusSpot(g, &temp, 'R', depth+1);
+            #ifdef AI_DEBUG
+            printf("GOT A RESULT\n");
+            printf("%s\n", *result);
+            #endif
+            if (*result[0] == 'x') {
+                result = _findNextVacantCampusSpot(g, &temp, 'L', depth+1);
+            }
+        } else {
+            #ifdef AI_DEBUG
+            printf("arc is owned by someone else :(\n");
+            #endif
+            // arc is owned by another uni.
+            //result = "x\0";
+            strncpy(temp, "x\0", 2);
+            result = (path*)strndup(temp, PATH_LIMIT);
+        }
+    } else {
+        #ifdef AI_DEBUG
+        printf("I've gone too damn far\n");
+        #endif
+        strncpy(temp, "x\0", 2);
+        result = (path*)strndup(temp, PATH_LIMIT);
+    }
+    //printf("result: %s\n", *result);
+    #ifdef AI_DEBUG
+    if (depth == 0) {
+        printf("Going to %s", *result);
+    }
+    #endif
+    return result;
+}
+static path *findNextVacantCampusSpot(Game g, path *startingPath) {
+    path* res = _findNextVacantCampusSpot(g, startingPath, 'R', 0);
+    if (*res[0] == 'x') {
+        free(res);
+        res = _findNextVacantCampusSpot(g, startingPath, 'L', 0);
+    }
+    #ifdef AI_DEBUG
+    printf("\n\nGoing to %s\n\n", *res);
+    #endif
+    return res;
 }
